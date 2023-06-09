9fd8847dec27c52a168fcffedfe9017fa1ae6ac6
Илья <socspec@yandex.ru>
2023-06-09 21:11:54 +0300

    1

--- src/users.py
+++ src/users.py
@@ -1,9 +1,23 @@
 from src.base_validate import Response
+from src.validation_schemes import UserList, UserRegistration, InvalidUserRegistration
 
 
 class UsersValidate(Response):
 
-    def user_assert_bad_request(self, statuse_code):
-        self.assert_status_code(400)
+    def user_bad_request(self):
+        if self.response_status == 200:
+            
+            print(f'status_code = {self.response_status}, respone = {self.response_json}')
+        elif self.response_status == 400:
+            print(f'status_code = {self.response_status}, respone = {self.response_json}')
+        else:
+            assert self.response_status != 400 or 200, 'Что то пошло не так.'
 
-        
+    def user_validate(self):
+        if self.response_status == 200:
+            if isinstance(self.response_json, list):
+                for item in self.response_json:
+                    UserRegistration.parse_obj(item)
+        else:
+            InvalidUserRegistration.parse_obj(self.response_json)
+
--- tests/test_users.py
+++ tests/test_users.py
@@ -4,21 +4,22 @@ from src.base_validate import Response
 from src.endpoints import UsersEndPoints
 from src.validation_schemes import UserList, UserRegistration, InvalidUserRegistration
 from src.data import UsersData
+from src.users import UsersValidate
 
 
-# def test_users_list():
-#     r = requests.get(url=UsersEndPoints.LIST_USERS)
-#     response = Response(r)
-#     response.assert_status_code(200)
-#     response.validate(UserList)
+def test_users_list():
+    r = requests.get(url=UsersEndPoints.LIST_USERS)
+    response = Response(r)
+    response.assert_status_code(200)
+    response.validate(UserList)
 
 
 def test_user_registration():
     r = requests.post(url=UsersEndPoints.USER_REGISTRATION, data=UsersData.USER_REGISTRATION_DATA)
-    response = Response(r)
-    response.assert_status_code(200)
+    response = UsersValidate(r)
+    response.user_bad_request()
     # требуется обработать статус 400, когда пользователь с такими данными уже есть
-    response.validate(UserRegistration)
+    response.user_validate()
 
 
 def test_negative_user_registration():
