0acec1db21b344e5f550d889ad6da1ae9561a52a
Илья <socspec@yandex.ru>
2023-07-06 17:22:50 +0300

    1

--- src/errors.py
+++ src/errors.py
@@ -1,3 +1,7 @@
 class ErrorMessaages():
     WRONG_STATUS_CODE = 'Received status code is not equal to expected.'
-    WRONG_VALIDATE_ERRORS = ['Enter a valid username. This value may contain only letters, numbers, and @/./+/-/_ characters.', 'Enter a valid email address.', 'Пользователь with this Почта already exists.', 'Пользователь with this Username already exists.', 'This field is required.', 'A valid integer is required.']
+    WRONG_VALIDATE_ERRORS = ['Enter a valid username. This value may contain only letters, numbers, and @/./+/-/_ characters.',
+                             'Enter a valid email address.',
+                             'Пользователь with this Почта already exists.',
+                             'Пользователь with this Username already exists.',
+                             'This field is required.', 'A valid integer is required.']
